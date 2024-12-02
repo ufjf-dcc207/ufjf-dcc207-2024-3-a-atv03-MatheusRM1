ded3ef879d928af7b1ed1eb6dda2316476c70f49
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-12-02 20:42:08 -0300

    adiciona condicional

--- src/Animal.tsx
+++ src/Animal.tsx
@@ -1,19 +1,19 @@
 import "./Animal.css";
 
-interface AnimalProps{
-    icone: string;
-    nome: string;
-    peso: number;
-    extincao: boolean;
+interface AnimalProps {
+  icone: string;
+  nome: string;
+  peso: number;
+  extincao: boolean;
 }
 
-export default function Animal({icone, nome, peso, extincao}: AnimalProps){
-    return(
-        <div className="animal">
-            <div className="icon">{icone}</div>
-            <div className="nome">{nome}</div>
-            <div className="peso">{peso.toLocaleString()}kg</div>
-            <div className="extincao">{extincao}</div>
-        </div>
-    )
+export default function Animal({ icone, nome, peso, extincao }: AnimalProps) {
+  return (
+    <div className="animal">
+      <div className="icon">{icone}</div>
+      <div className="nome">{nome}</div>
+      <div className="peso">{peso.toLocaleString()}kg</div>
+      {extincao && <div className="extincao">Em extincao!</div>}
+    </div>
+  );
 }
