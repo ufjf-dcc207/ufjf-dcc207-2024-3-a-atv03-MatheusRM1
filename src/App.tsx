ce8189681b6d84c25c4217ca02c4d3e7a0f31279
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-12-05 22:11:33 -0300

    adiciona filho na exibicao

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,22 @@ import Exibicao from "./Exibicao";
 function App() {
   return (
     <div className="app">
-      <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T12:00-03:00")} cercado="A1"/>
-      <Animal icone="🦁" nome="Leão" peso={190.5} extincao={true} />
-      <Animal icone="🐼" nome="Panda" peso={400.5} extincao={true} />
-      <Exibicao abertura={new Date("2024-12-06T08:00:00.000-03:00")} fechamento={new Date("2024-12-06T17:00-03:00")} cercado="B1"/>
-      <Animal icone="🦄" nome="Unicornio" peso={500.25} extincao={true} />
+      <Exibicao
+        abertura={new Date("2024-12-06T08:00:00.000-03:00")}
+        fechamento={new Date("2024-12-06T12:00-03:00")}
+        cercado="A1"
+      >
+        <Animal icone="🦁" nome="Leão" peso={190.5} extincao={true} />
+        <Animal icone="🐼" nome="Panda" peso={400.5} extincao={true} />
+      </Exibicao>
+
+      <Exibicao
+        abertura={new Date("2024-12-06T08:00:00.000-03:00")}
+        fechamento={new Date("2024-12-06T17:00-03:00")}
+        cercado="B1"
+      >
+        <Animal icone="🦄" nome="Unicornio" peso={500.25} extincao={true} />
+      </Exibicao>
     </div>
   );
 }
